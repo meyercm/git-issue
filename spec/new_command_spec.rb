25d70591c46f3fe0c735724ab1c691ac58ad6763
Chris Meyer <meyer.cm@gmail.com>
2015-09-09 06:00:58 -0400

    more tests for new_command, bugfixes

--- spec/new_command_spec.rb
+++ spec/new_command_spec.rb
@@ -5,6 +5,34 @@ describe GitIssue::NewCommand do
     allow(subject).to receive(:write_output).and_return(nil)
   end
   describe '#parse' do
+    it "can parse message" do
+      args = '-m "test_message"'
+      result = subject.parse(args.split(" "))
+      expect(result.message).to eq('"test_message"')
+    end
+    it "can parse tags" do
+      tagobj = Object.new
+      expect(GitIssue::Tag).to receive(:parse).with("milestone:2.0.0").and_return(tagobj)
+      args = '-t milestone:2.0.0'
+      result = subject.parse(args.split(" "))
+      expect(result.tags).to eq(tagobj)
+    end
+    it "can parse a default tag" do
+      tagobj = Object.new
+      expect(GitIssue::GitWorker).to receive(:default_tag).and_return("default")
+      expect(GitIssue::Tag).to receive(:parse).with("default:blah").and_return(tagobj)
+      result = subject.parse(["blah"])
+      expect(result.tags).to eq(tagobj)
+    end
+    it "can parse default tag and -t tags" do
+      args = 'feature -t milestone:2.0.0'
+      tagobj = Object.new
+      expect(GitIssue::GitWorker).to receive(:default_tag).and_return("default")
+      expect(GitIssue::Tag).to receive(:parse).with("milestone:2.0.0,default:feature").and_return(tagobj)
+      result = subject.parse(args.split(" "))
+      expect(result.tags).to eq(tagobj)
+    end
+
   end
   describe '#run' do
     before do
